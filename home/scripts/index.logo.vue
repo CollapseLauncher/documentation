274fa3f7d3b3e7d3105f24a53ac90a2b1c2c3995
Bagus Nur Listiyono <dzakibagus@gmail.com>
2024-12-15 01:21:46 +0700

    adjust vtuber logo width

--- home/scripts/index.logo.vue
+++ home/scripts/index.logo.vue
@@ -3,7 +3,7 @@
         <div style="display: flex; flex-wrap: wrap; justify-content: center; align-items: center;">
             <div style="text-align: center; margin: 10px;">
                 <div style="display: flex; flex-direction: column; align-items: center;">
-                    <img width="256px" height="auto" src="/img/vtuber.png" /><br />
+                    <img width="350px" height="auto" src="/img/vtuber.png" /><br />
                     <i>I know, it's not a good one. But at least we made it lol</i><br />
                     <i>~ neon-nyan</i>
                 </div>
