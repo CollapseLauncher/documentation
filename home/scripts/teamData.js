c35249a4b0b99a407c01167de0a6f063b82e789a
Bagus Nur Listiyono <dzakibagus@gmail.com>
2024-12-17 14:07:51 +0700

    [team] fix some github users and optimize images

--- home/scripts/teamData.js
+++ home/scripts/teamData.js
@@ -2,35 +2,35 @@ export const coreDevelopers = [
     {
         name: "Kemal Setya Adhi",
         username: "@neon-nyan",
-        image: "https://avatars.githubusercontent.com/u/30566970?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/30566970?v=4&s=100",
         github: "https://github.com/neon-nyan",
         role: "Founder, RE, .NET Expert, WinAPI Expert"
       },
       {
         name: "Bagus Nur Listiyono",
         username: "@bagusnl",
-        image: "https://avatars.githubusercontent.com/u/28079733?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/28079733?v=4&s=100",
         github: "https://github.com/bagusnl",
         role: "Co-Project Manager, Integration & DevOps Manager"
       },
       {
         name: "Ron Friedman",
         username: "@cryotechnic",
-        image: "https://avatars.githubusercontent.com/u/9833218?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/9833218?v=4&s=100",
         github: "https://github.com/Cryotechnic",
         role: "Co-Project Manager, DevOps Consultant"
       },
       {
         name: "Shatyuka",
         username: "@shatyuka",
-        image: "https://avatars.githubusercontent.com/u/31368738?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/31368738?v=4&s=100",
         github: "https://github.com/shatyuka",
         role: "C/C++ Native Expert, WinUI Consultant"
       },
       {
         name: "Gabriel Lima",
         username: "@gablm",
-        image: "https://avatars.githubusercontent.com/u/44784408?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/44784408?v=4&s=100",
         github: "https://github.com/gablm",
         role: "Inter-app Integration Expert, Accessibility Consultant"
       }
@@ -39,98 +39,98 @@ export const coreDevelopers = [
 export const contributors = [
     {
         name: "Kylian Nicouleaud",
-        image: "https://avatars.githubusercontent.com/u/54137141?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/54137141?v=4&s=80",
         link: "https://github.com/Kiki79250CoC",
         contributions: "🌍 Translation, 🐛 Bug Reports"
       },
       {
         name: "misaka10843",
-        image: "https://avatars.githubusercontent.com/u/69132853?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/69132853?v=4&s=80",
         link: "https://misaka.sakurakoi.top/",
         contributions: "🌍 Translation"
       },
       {
         name: "Vermilion-Sinsha",
-        image: "https://avatars.githubusercontent.com/u/131636335?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/131636335?v=4&s=80",
         link: "https://github.com/Vermilion-Sinsha",
         contributions: "🌍 Translation"
       },
       {
         name: "Pham Khanh Nhan",
-        image: "https://avatars.githubusercontent.com/u/78801337?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/78801337?v=4&s=80",
         link: "https://github.com/kleqing",
         contributions: "🌍 Translation"
       },
       {
         name: "DeepChirp",
-        image: "https://avatars.githubusercontent.com/u/66902050?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/66902050?v=4&s=100",
         link: "https://github.com/DeepChirp",
         contributions: "🌍 Translation"
       },
       {
         name: "Faelayis",
-        image: "https://avatars.githubusercontent.com/u/48393914?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/48393914?v=4&s=80",
         link: "https://github.com/Faelayis",
         contributions: "🌍 Translation"
       },
       {
         name: "Hasukay",
-        image: "https://avatars.githubusercontent.com/u/22459107?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/22459107?v=4&s=80",
         link: "https://twitter.com/hasukay__",
         contributions: "🌍 Translation"
       },
       {
         name: "Kajitsy",
-        image: "https://avatars.githubusercontent.com/u/94784342?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/94784342?v=4&s=80",
         link: "https://github.com/Kajitsy",
         contributions: "📖 Documentation, 🌍 Translation"
       },
       {
         name: "despenser08",
-        image: "https://avatars.githubusercontent.com/u/50207925?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/50207925?v=4&s=80",
         link: "https://github.com/despenser08",
         contributions: "🌍 Translation"
       },
       {
         name: "seria",
-        image: "https://avatars.githubusercontent.com/u/61446626?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/61446626?v=4&s=80",
         link: "https://seria.is-a.dev/",
         contributions: "🌍 Translation"
       },
       {
         name: "xTaiwanPingLord",
-        image: "https://avatars.githubusercontent.com/u/52948923?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/52948923?v=4&s=80",
         link: "https://github.com/xTaiwanPingLord",
         contributions: "🌍 Translation"
       },
       {
         name: "Scald",
-        image: "https://avatars.githubusercontent.com/u/104459145?v=4?s=100",
-        link: "https://github.com/Scald",
+        image: "https://avatars.githubusercontent.com/u/104459145?v=4&s=80",
+        link: "https://github.com/Arikatsu",
         contributions: "💻 Code"
       },
       {
         name: "Iskandar Montano",
-        image: "https://avatars.githubusercontent.com/u/12277635?v=4?s=100",
-        link: "https://github.com/Iskandar-Montano",
+        image: "https://avatars.githubusercontent.com/u/12277635?v=4&s=80",
+        link: "https://github.com/muscularcandy67",
         contributions: "💻 Code"
       },
       {
         name: "Scighost",
-        image: "https://avatars.githubusercontent.com/u/61003590?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/61003590?v=4&s=80",
         link: "https://github.com/Scighost",
         contributions: "🐛 Bug Reports"
       },
       {
         name: "IhoFox",
-        image: "https://avatars.githubusercontent.com/u/133899447?v=4?s=100",
+        image: "https://avatars.githubusercontent.com/u/133899447?v=4&s=80",
         link: "https://github.com/IhoFox",
         contributions: "🌍 Translation"
       },
       {
         name: "kujou",
-        image: "https://avatars.githubusercontent.com/u/23724383?v=4?s=100",
-        link: "https://github.com/kujou",
+        image: "https://avatars.githubusercontent.com/u/23724383?v=4&s=80",
+        link: "https://github.com/kujou-kju",
         contributions: "🌍 Translation"
       }
 ]
