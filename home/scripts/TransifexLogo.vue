4ce2a393e64dad6e42fe672cd78cce554ed15a80
shatyuka <shatyuka@qq.com>
2024-12-18 17:22:52 +0800

    Change the color of transifex logo
    to make it more suitable for dark mode

--- home/scripts/TransifexLogo.vue
+++ home/scripts/TransifexLogo.vue
@@ -11,16 +11,5 @@
 <style scoped>
 .logo {
   height: 80px;
-  box-sizing: border-box; /* Ensure the border is included in the element's total width and height */
-}
-
-html.dark .logo {
-  border: 5px solid #f9f9f9; /* Add a light border in dark mode */
-  background-color: #f9f9f9; /* Fill the border with a light background color in dark mode */
-}
-
-html:not(.dark) .logo {
-  border: 5px solid transparent; /* No border in light mode */
-  background-color: transparent; /* No background color in light mode */
 }
 </style>
