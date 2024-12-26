5a7b20aeea29fdb0f5b882cbf5e03f3be6666906
Ron Friedman <cryogen65@gmail.com>
2024-12-26 00:35:42 -0500

    Create copyright component

---
+++ home/scripts/copyright.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="footer">
+            <p>&nbsp; &copy; 2024 - {{ currentYear }}. All rights reserved.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentYear: new Date().getFullYear()
+        };
+    }
+};
+</script>
+
+<style scoped>
+.footer {
+    position: relative;
+    width: 100%;
+    text-align: center;
+}
+</style>
